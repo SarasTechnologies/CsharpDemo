24499297abe6e8523061d9fe0dd6780c24b72931
Satheesh Kumar <satheesh2k6@hotmail.com>
2023-02-11 18:54:36 +0530

    Class demo

---
+++ ClassDemo/Employee.cs
@@ -0,0 +1,33 @@
+﻿public class Employee
+{
+    // Field
+    private static string defaultStatus = "Hired";
+
+    // Property
+    public string FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string Address { get; set; }
+	public int Age { get; set; }
+	public string ContactNumber { get; set; }
+    public string Status { get; set; } = defaultStatus; // Assigned default values to property
+
+    // Constructor
+	public Employee(string firstName, string address, int age, string contactNumber)
+    {
+        FirstName = firstName;
+        Address = address;
+        Age = age;
+        ContactNumber = contactNumber;
+    }
+
+    // Method
+    public bool IsHired()
+    {
+        if (Status == "Hired")
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
